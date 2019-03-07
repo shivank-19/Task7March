b56f390c4d506e1c922e7092c4cf63c916756919
shivankchauh <shivank.chauhan_mca18@gla.ac.in>
2019-03-07 10:53:01 +0530

    init commit

---
+++ app/src/main/java/com/e/task7march/MainActivity.java
@@ -0,0 +1,13 @@
+package com.e.task7march;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
